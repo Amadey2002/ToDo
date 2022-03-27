9c8a4b104934a745387456ed04fde64304e1d148
Amadey2002 <55347723+Amadey2002@users.noreply.github.com>
2022-03-27 18:45:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+          function ins(){
+            var newList = prompt("Новое дело:", "Введите дело")
+             alert("1: "+newList);
+                    };
+        elemIns.addEventListener("click", ins);
+
+        function edt(){
+            var newList = prompt("Новое дело:", 1);
+             alert(newList);
+                    };
+        elemEdt.addEventListener("click", edt);
+
+        function del(){
+            var newList = prompt("Новое дело:", 1);
+             alert(newList);
+                    };
+        elemDel.addEventListener("click", del);
+
+        function save(){
+            var newList = prompt("Новое дело:", 1);
+             alert(newList);
+                    };
+        elemSave.addEventListener("click", save);
